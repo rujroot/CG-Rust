f41046b14e182979a86d21d44c5c7565102879e3
Rujroot <rujroot@gmail.com>
2025-10-31 11:22:13 +0700

    single

--- lab81-mandelbrot-single/src/main.rs
+++ lab81-mandelbrot-single/src/main.rs
@@ -1,4 +1,4 @@
-use image::{ ImageBuffer, Rgb };
+use image::{ ImageBuffer, Pixel, Rgb };
 use std::time::Instant;
 use num_complex::Complex;
 use hsv_to_rgb::hsv_to_rgb;
@@ -8,7 +8,7 @@ fn main() {
     let image_height:u32 = 1080;
     let max_iterations:u32 = 1000;
 
-    let mut imgbuf = ImageBuffer::new(image_width, image_height);
+    let mut imgbuf: ImageBuffer<Rgb<u8>, Vec<u8>> = ImageBuffer::new(image_width, image_height);
 
     let x_min:f64 = -2.0;
     let x_max:f64 = 1.0;
@@ -18,9 +18,29 @@ fn main() {
     let start = Instant::now();
     for y in 0..image_height {
         for x in 0..image_width {
-            // TODO: Optimize mapping from pixel to complex plane
+            let c_real = x_min + (x as f64 / image_width as f64) * (x_max - x_min);
+            let c_imag = y_min + (y as f64 / image_height as f64) * (y_max - y_min);
+            let c = Complex::new(c_real, c_imag);
 
-            let pixel: Rgb<u8> = Rgb([0, 0, 0]);
+            let mut z = Complex::new(0.0, 0.0);
+            let mut iterations = 0;
+            while z.norm_sqr() <= 4.0 && iterations < max_iterations {
+                z = z * z + c;
+                iterations += 1;
+            }
+
+            let pixel:Rgb<u8> = if iterations == max_iterations {
+                let result: Rgb<u8> = Rgb([0, 0, 0]);
+                result
+            } else {
+                let t = iterations as f32 / max_iterations as f32;
+                let hue = (t * 360.0 * 3.0) % 360.0;
+                let saturation = 0.8;
+                let value = if t < 0.5 { 0.5 + t } else { 1.0 };
+                let result: Rgb<u8> = hsv_to_rgb(hue, saturation, value);
+                result
+            };
+            
             imgbuf.put_pixel(x, y, pixel);
         }
     }
